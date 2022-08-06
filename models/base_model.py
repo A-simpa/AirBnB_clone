318508c04b9f062c290925204a54d605362f8d7e
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-08-06 01:00:15 +0000

    updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,17 @@ from datetime import datetime
 
 class BaseModel:
     """ The BaseModel class for console """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for attr, value in kwargs.items():
+                if attr == "created_at" or attr == "updated_at":
+                    self.__dict__[attr] = datetime.fromisoformat(value)
+                elif attr != "__class__":
+                    self.__dict__[attr] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return (f"[BaseModel] ({self.id}) {self.__dict__}")
@@ -26,4 +33,3 @@ class BaseModel:
         rep["updated_at"] = rep["updated_at"].isoformat()
         rep["created_at"] = rep["created_at"].isoformat()
         return rep
-
