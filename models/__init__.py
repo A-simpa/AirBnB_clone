32acf155f137e6dacd74e3351b6b07aee04c6ece
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-08-08 13:50:45 +0000

    added test for review

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,8 @@ from models.engine.file_storage import FileStorage
 from models.city import City
 from models.state import State
 from models.amenity import Amenity
+from models.review import Review
+from models.user import User
 
 storage = FileStorage()
 storage.reload()
