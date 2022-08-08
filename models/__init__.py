2f6ca8a2cdb7f7aa96f97b329036afd0a342d051
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-08-08 12:29:48 +0000

    added two tests

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ initializes the module models """
 from models.engine.file_storage import FileStorage
+from models.city import City
+from models.state import State
 
 storage = FileStorage()
 storage.reload()
